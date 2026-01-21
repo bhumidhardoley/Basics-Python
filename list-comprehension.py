0be2f7dfbab7e49c70108d99d74f8b88a7bd9b4f
bhumidhardoley <bhumidhar414@gmail.com>
2026-01-21 08:42:21 +0530

    List comprehension

---
+++ list-comprehension.py
@@ -0,0 +1,21 @@
+# list Comprehension = short and clean way to create a collection 
+# Squaring every number in a list
+
+a = [ 1, 2, 3, 4, 5]
+result = [ value **2 for value in a ]  # it creates a list comprehension to create a new list by squaring each number in a 
+print(result)
+
+# Example with for loop 
+
+a = [ 1, 2, 3, 4, 5]
+res = [ ] # creates an empty list to store results
+for val in a:
+    res.append(val**2)
+print(res)
+
+
+# Conditional statements in list comprehension
+
+numbers = [1, 2, 3, 4, 5]
+res = [ value**2 for value in a if value % 2 == 0 ]
+print(res)
