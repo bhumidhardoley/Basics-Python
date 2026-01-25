336601e3ef4332f6698d3ea82d7435e590499bba
bhumidhardoley <bhumidhar414@gmail.com>
2026-01-25 08:38:10 +0530

    Completeed basics of functions

--- functions-variable.py
+++ functions-variable.py
@@ -1,3 +1,4 @@
+
 # # Functions in simple term are the actions that a computer language will know how to perform 
 # # For example the printf() is a function that takes arguments .strings and displays the strings
 # # Another example can be the input() in here it takes arguments and can take input and can also return values 
@@ -34,20 +35,49 @@
 
 # Float calculations simple
 
-x = float(input("Enter a number: "))
-y = float(input("Enter a number: "))
+# x = float(input("Enter a number: "))
+# y = float(input("Enter a number: "))
 
-z = round(x+y,3) # In here when useing the 3 it only counts towards the decimal points
+# z = round(x+y,3) # In here when useing the 3 it only counts towards the decimal points
 
-# If we want to add an comma in the answer then we can follow the 
-print(f"{z:,}")
+# # If we want to add an comma in the answer then we can follow the 
+# print(f"{z:,}")
 
-# : the formatting starts from here , is a format specifier which adds comma in every thousands
+# # : the formatting starts from here , is a format specifier which adds comma in every thousands
 
-newZ = x+y
-print(f"{newZ:,.2f}") # .2f make the decimal point 2 digit long only
+# newZ = x+y
+# print(f"{newZ:,.2f}") # .2f make the decimal point 2 digit long only
 
-# And also we can pad the answer with 0 for example in format specifier we can add 015 means width 15 and padd with 0
+# # And also we can pad the answer with 0 for example in format specifier we can add 015 means width 15 and padd with 0
 
-# print(f"{z:015,}") 
-print(f"{newZ:,.2%}")  # multiplies the numer with 100 and add % after 2 digits
+# # print(f"{z:015,}") 
+# print(f"{newZ:,.2%}")  # multiplies the numer with 100 and add % after 2 digits
+
+# We can define our own functions using def keyword
+
+def add_numbers(x , y ):
+    return x+y
+
+sum = add_numbers(2,3)
+
+print(f"Summation is {sum}")
+
+def welcome(name):
+    print(f"Hello,{name}")
+
+welcome('Bhumidhar')
+
+# Now in this topic if we want to make multiple fuctions we can use
+
+def main():
+    print("Welcome")
+    welcome('Bhumidhar')    # It can be seen as a the main code when we want to define mulltiple functions
+main()
+
+def square(num):
+    return num*num
+
+def mulltipl(num):
+    newNum = square(num)
+    print(f"The square of the number is : {newNum}")
+mulltipl(45)
